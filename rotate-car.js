7c7af93605829e9f06c025a04e0359d922b97295
Sh0rti <89410201+therealsh0rti@users.noreply.github.com>
2022-04-02 12:05:21 +0200

    Added spinning animation at random

--- rotate-car.js
+++ rotate-car.js
@@ -1,4 +1,29 @@
 window.addEventListener('load', (event) => {
-    var renderedcar = document.querySelector('img[src*="render-vehicle"]');
-    renderedcar.style.transform = "rotate(" + (Math.random() * 360).toString() + "deg)";
+    var renderedCar = document.querySelector('img[src*="render-vehicle"]');
+    var chance = Math.random() * 100;
+    if (chance > 75)
+    {
+        renderedCar.style.animationName = "teslascope_car_rotator_spin";
+        renderedCar.style.animationIterationCount = "infinite";
+        var duration;
+        var timing;
+
+        chance = Math.random() * 100;
+        if (chance > 50)
+        {
+            duration = "3000ms";
+            timing = "ease-in-out";
+        }
+        else
+        {
+            duration = (Math.random() * 10000).toString() + "ms";
+            timing = "linear";
+        }
+        renderedCar.style.animationDuration = duration;
+        renderedCar.style.animationTimingFunction = timing;
+    }
+    else
+    {
+        renderedCar.style.transform = "rotate(" + (Math.random() * 360).toString() + "deg)";
+    }
 });
